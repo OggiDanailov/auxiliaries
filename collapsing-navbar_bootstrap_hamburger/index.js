9fa0e40c2a0d4b82050a96646be24d0d26ec7911
Oggi Danailov <oxd017@jefferson.edu>
2019-04-19 16:25:03 -0400

    slding gallery created; video creted as well

--- collapsing-navbar_bootstrap_hamburger/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	const hamburger = document.getElementsByClassName("hamburger")[0]
-
-	hamburger.addEventListener('click', function(){
-		console.log('asdfasdfasdf')
-		hamburger.classList.toggle("is-active")
-	})
-})
