f3c052282c68360cd456bc9a6fe9383d4e7357af
Oggi Danailov <oxd017@jefferson.edu>
2019-04-23 17:14:38 -0400

    before going home

---
+++ third-one/index.js
@@ -0,0 +1,18 @@
+const radios = document.getElementsByClassName('radios')
+const navDot = document.getElementsByClassName("nav-dot")
+const internalWrapper = document.getElementsByClassName("internal-wrapper")[0]
+
+// window.addEventListener('load', function(){
+// 	console.log('asdfasdf')
+// 	for(let i =0;i<radios.length;i++){
+// 		if(radios[i].checked){
+// 			console.log(radios[i].checked)
+// 			navDot[i].style.backgroundColor = '#219FD6'
+// 		}
+// 	}
+// })
+
+
+
+
+
