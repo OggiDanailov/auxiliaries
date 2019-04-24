8d066826fd2c379d75495eee0b69970bd34ff3d5
Oggi Danailov <oxd017@jefferson.edu>
2019-04-24 17:08:35 -0400

    big gallery

--- slide-show/index.js
+++ slide-show/index.js
@@ -1,11 +1,5 @@
-$('#carouselExampleControls').carousel({
-  interval: false
-})
 
-
-
-
-
+// this is the default value - image 1
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -14,23 +8,59 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+  } 
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none"; 
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+  
   slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+  // dots[slideIndex-1].className += " active";
 }
+
+
+$('.multi-item-carousel').carousel({
+  interval: false
+});
+
+
+
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$('.multi-item-carousel .carousel-item').each(function(){
+  var next = $(this).next();
+
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  } else {
+    $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
