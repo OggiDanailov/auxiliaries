c3d09859779184932174f2359c33db6d6d0157f2
OggiDanailov <ogidan@abv.bg>
2019-04-27 22:39:55 -0400

    the collapsing image gallery resolved

--- slick/index.js
+++ slick/index.js
@@ -1,41 +1,42 @@
-$(document).ready(function(){
-	let size = window.matchMedia("all and (min-width: 992px)")
-
-	if(size.matches){
-		updateContainer();	
-	}
-	
-
+$(document).ready(function(){		
+	$('.slider').slick({
+	    name: 'whatever',
+	    autoplay: false,
+	    prevArrow: $(".prev"),
+	    nextArrow: $(".next"),
+	    dots: false,
+	    rows: 1,
+	     responsive: [
+    {
+      breakpoint: 2000,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+    });	
 });
 
-function updateContainer(){
-		console.log('wahtever')
-			$('.slider').slick({
-	        name: 'whatever',
-	         autoplay: false,
-	         prevArrow: $(".prev"),
-	         nextArrow: $(".next"),
-	         dots: false,
-	         rows: 1,
-	         slidesToShow: 4
-	      	});
-}
 
-
-
-
-
-var moveButton = document.querySelector('.move-button');
-var items = document.querySelectorAll('.grid-a .grid-item');
-
-moveButton.onclick = function() {
-  for ( var i=0; i < items.length; i++ ) {
-    items[i].classList.toggle('is-moved');
-  }
-};
-				
-	
-			
+		
 
 
 		   
