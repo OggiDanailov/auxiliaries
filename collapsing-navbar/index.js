c00ecda42637621840ec4ef9a640be941b6bc30b
Oggi Danailov <oxd017@jefferson.edu>
2019-04-29 13:43:38 -0400

    collapsing navbar 2 added

--- collapsing-navbar/index.js
+++ collapsing-navbar/index.js
@@ -1,7 +1,20 @@
-let mainNav = document.getElementById('js-menu');
+let mainNav = document.getElementsByClassName('main-nav')[0]
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
+let button = document.getElementsByClassName("button")[0]
+let invisible = document.getElementsByClassName('invisible')[0];
+let sq = document.getElementById('sq');
+
+
 navBarToggle.addEventListener('click', function () {
-	console.log('whatever')
   mainNav.classList.toggle('active');
+  mainNav.classList.toggle("bordering")
+  console.log(mainNav)
+
 });
+
+button.addEventListener("click", function(){
+	invisible.classList.toggle('active')
+	invisible.classList.toggle("bordering")
+	console.log(invisible)
+})
