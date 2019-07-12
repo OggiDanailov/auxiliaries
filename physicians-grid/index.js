beb54c22e9ce1bf3241b18cf008dfd31f8d65432
OggiDanailovLab <oxd017@jefferson.edu>
2019-07-12 15:42:22 -0400

    physicians redo

--- physicians-grid/index.js
+++ physicians-grid/index.js
@@ -1,6 +1,10 @@
 var readMore = document.getElementsByClassName("read-more")
 var readLess = document.getElementsByClassName("read-less")
 var invisible = document.getElementsByClassName("invisible")
+var invisibleMobile = document.getElementsByClassName("invisible-mobile")
+
+var seeLess = document.getElementsByClassName('arrow-up')[0];
+var seeAll = document.getElementsByClassName('arrow-down')[0];
 
 
 for(let i = 0;i<readMore.length;i++){
@@ -16,6 +20,28 @@ for(let i = 0;i<readLess.length;i++){
 		readMore[i].style.display = 'block'
 	})
 
+}
+
+
+if(seeAll){
+  seeAll.addEventListener('click', function(){
+  for(let i = 0;i<invisibleMobile.length;i++){		
+  		invisibleMobile[i].classList.add("mobile-hidden-visible");
+  	}  
+  seeAll.style.display = 'none';
+  seeLess.style.display = 'block';
+  })  
+}
+
+if(seeLess){
+  seeLess.addEventListener("click", function(){
+  	for(let i = 0;i<invisibleMobile.length;i++){		
+  		invisibleMobile[i].classList.remove("mobile-hidden-visible")
+  	}
+  
+  seeAll.style.display = 'block'
+  seeLess.style.display = 'none';
+  });  
 }
 
 
